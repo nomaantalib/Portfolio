9bc4dd601fc8477e186517b51ebce6c176d5f5e7
nomaantalib <mohdnomaantalib@gmail.com>
2026-01-02 20:30:55 +0530

    Frontend CORS origin url fix

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://portfolio-lfjn.onrender.com/"],
     credentials: true,
   })
 );
